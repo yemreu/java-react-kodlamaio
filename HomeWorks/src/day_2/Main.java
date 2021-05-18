75e42bfdbfd21ef6183966b22054bca4bb442e67
yemreu <yunusemreuzun@outlook.com>
2021-05-18 18:51:24 +0300

    Day 2 - Homework 1

--- HomeWorks/src/day_2/Main.java
+++ HomeWorks/src/day_2/Main.java
@@ -1,11 +1,29 @@
 package day_2;
 
+import java.util.Iterator;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Instructor enginHoca = new Instructor(1, "Engin Demiroğ", "Engin Demiroğ is a Udemy instructor with educational courses available for enrollment.");
 		
 		Course javaReact = new Course(1, "Yazılım Geliştirici Yetiştirme Kampı (JAVA & REACT)", "2 ay sürecek ücretsiz ve profesyonel bir programla, sıfırdan yazılım geliştirme öğreniyoruz.", enginHoca);
+		
+		System.out.println("------Instructors------");
+		Instructor[] instructors = {enginHoca};
+		
+		for (Instructor instructor : instructors) {
+			System.out.println(instructor.id + ". " + instructor.name);
+		}
+		
+		System.out.println("\n------Courses------");
+		Course[] courses = {javaReact};
+		
+		for (Course course : courses) {
+			System.out.println(course.id + ". " + course.name);
+		}
+		
+		System.out.println();
 		
 		CourseManager courseManager = new CourseManager();
 		courseManager.enrollInCourse(javaReact);
