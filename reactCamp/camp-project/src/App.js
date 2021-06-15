bb6c8df0f20f6d993743707cec99cc4709ec9e58
yemreu <yunusemreuzun@outlook.com>
2021-06-15 20:19:06 +0300

    Day 11 - React Intro
    @CrossOrigin - CORS

---
+++ reactCamp/camp-project/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Dashboard from "./layout/Dashboard";
+import Navi from "./layout/Navi";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+
+function App() {
+  return (
+    <div className="App">
+      <Navi/>
+      <Container className = "main">
+        <Dashboard />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
