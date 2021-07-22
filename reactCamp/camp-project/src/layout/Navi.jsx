7769262318cd01e6646858d3b217aa4dd112fc9a
yemreu <yunusemreuzun@outlook.com>
2021-07-22 18:37:02 +0300

    Day 13 - Redux

--- reactCamp/camp-project/src/layout/Navi.jsx
+++ reactCamp/camp-project/src/layout/Navi.jsx
@@ -4,8 +4,10 @@ import { Menu, Container } from "semantic-ui-react";
 import CartSummary from "./CartSummary";
 import SignedIn from "./SignedIn";
 import SignedOut from "./SignedOut";
+import { useSelector } from "react-redux";
 
 export default function Navi() {
+  const { cartItems } = useSelector((state) => state.cart);
   const [isAuthenticated, setIsAuthenticated] = useState(true)
   const history = useHistory();
 
@@ -25,7 +27,7 @@ export default function Navi() {
           <Menu.Item name="home" />
           <Menu.Item name="messages" />
           <Menu.Menu position="right">
-            <CartSummary/>
+            {cartItems.length > 0 && <CartSummary/>}
             {isAuthenticated?(<SignedIn signOut={handleSignOut}/>):(<SignedOut signIn={handleSignIn}/>)}
           </Menu.Menu>
         </Container>
--- reactCamp/camp-project/src/pages/ProductDetail.jsx
+++ reactCamp/camp-project/src/pages/ProductDetail.jsx
@@ -11,7 +11,7 @@ export default function ProductDetail() {
   useEffect(() => {
     let productService = new ProductService();
     productService.getByProductName(name).then(result => setProduct(result.data.data))
-  },[])
+  },[name])
 
   return (
     <div>
